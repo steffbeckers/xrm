5fdc50f760f2961b41fdd5d944e80ae31681b66f
Steff Beckers <steff@steffbeckers.eu>
2021-03-10 19:23:57 +0100

    Removed back-end

--- back-end/src/Application/Common/Interfaces/ICurrentUserService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace XRM.Application.Common.Interfaces
-{
-    public interface ICurrentUserService
-    {
-        string UserId { get; }
-    }
-}
--- back-end/src/Application/Common/Interfaces/IDateTime.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace XRM.Application.Common.Interfaces
-{
-    public interface IDateTime
-    {
-        DateTime Now { get; }
-    }
-}
